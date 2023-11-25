39b568a90bc5acdd35ac495579ed28e93fa09ca1
Kesley Keven <96406090+kesley-developer@users.noreply.github.com>
2023-11-25 20:36:57 -0300

    Add files via upload

---
+++ assets/autoText.js
@@ -0,0 +1,8 @@
+function autoText(){
+    var typed = new Typed('#auto-text', {
+        strings: ['<h3>Viagens, lugares e muito conforto</h3>', '<h3>São as oportunidades da vida e que nós oferecemos</h3>','<h3>Aproveite a vida com a "NOME DA EMPRESA"</h3>'],
+        typeSpeed: 50,
+        showCursor: false
+    })    
+}
+autoText()
